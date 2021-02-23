39be8a30fa83434fab18b83bd8b8ea20f28b43d8
Marie-Make <75613302+Marie-Make@users.noreply.github.com>
2021-02-23 14:02:42 +0100

    Update cyclegan_datasets.py

--- cyclegan_datasets.py
+++ cyclegan_datasets.py
@@ -14,6 +14,6 @@ DATASET_TO_IMAGETYPE = {
 
 """The path to the output csv file."""
 PATH_TO_CSV = {
-    'WFG2FG_train': './mon-projet/input/WFG2FG_train.csv',
+    'WFG2FG_train': './project/input/WFG2FG_train.csv',
     
 }
