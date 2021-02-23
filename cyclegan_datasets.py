5a8c4b7915878f9d5a41ad90ea770537800c6c72
Marie-Make <75613302+Marie-Make@users.noreply.github.com>
2021-02-23 13:59:29 +0100

    Add files via upload

---
+++ cyclegan_datasets.py
@@ -0,0 +1,19 @@
+"""Contains the standard train/test splits for the cyclegan data."""
+
+"""The size of each dataset. Usually it is the maximum number of images from
+each domain."""
+DATASET_TO_SIZES = {
+    'WFG2FG_train': 1502
+}
+
+"""The image types of each dataset. Currently only supports .jpg or .png"""
+DATASET_TO_IMAGETYPE = {
+    'WFG2FG_train': '.jpg',
+    
+}
+
+"""The path to the output csv file."""
+PATH_TO_CSV = {
+    'WFG2FG_train': './mon-projet/input/WFG2FG_train.csv',
+    
+}
