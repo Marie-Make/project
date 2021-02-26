579cd87f51c959a1b51196b1a8ba6108aa11489c
Marie-Make <75613302+Marie-Make@users.noreply.github.com>
2021-02-26 13:23:51 +0100

    Update layers.py

--- layers.py
+++ layers.py
@@ -16,7 +16,7 @@ def instance_norm(x):
 
     with tf.compat.v1.variable_scope("instance_norm"):
         epsilon = 1e-5
-        mean, var = tf.np.moments(x, [1, 2], keep_dims=True)
+        mean, var = tf.nn.moments(x, [1, 2], keep_dims=True)
         scale = tf.get_variable('scale', [x.get_shape()[-1]],
                                 initializer=tf.truncated_normal_initializer(
                                     mean=1.0, stddev=0.02
@@ -48,7 +48,7 @@ def general_conv2d(inputconv, o_d=64, f_h=7, f_w=7, s_h=1, s_w=1, stddev=0.02,
 
         if do_relu:
             if(relufactor == 0):
-                conv = tf.np.relu(conv, "relu")
+                conv = tf.nn.relu(conv, "relu")
             else:
                 conv = lrelu(conv, relufactor, "lrelu")
 
@@ -76,7 +76,7 @@ def general_deconv2d(inputconv, outshape, o_d=64, f_h=7, f_w=7, s_h=1, s_w=1,
 
         if do_relu:
             if(relufactor == 0):
-                conv = tf.np.relu(conv, "relu")
+                conv = tf.nn.relu(conv, "relu")
             else:
                 conv = lrelu(conv, relufactor, "lrelu")
 
